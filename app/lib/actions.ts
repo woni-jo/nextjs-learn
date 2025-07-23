988cf5dc654bb03b017b2c4a4889934cc6909f76
woni-jo <woni0517@naver.com>
2025-07-23 15:13:54 +0900

    Delete 수정

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -112,7 +112,7 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
+  // throw new Error('Failed to Delete Invoice');
 
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
